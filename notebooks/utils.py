6fca7ff858a7104c27d0b6176dd21e578db4df60
Surya Sumukh <ss3345@cornell.edu>
2017-07-29 10:22:05 -0700

    feat: Added functions to load train and test data and extract bottleneck features from vgg16
    The Train/Test generators are too slow. Instead wrote functions to read and extract
    convolution features from vgg16. This processed data will be streamed by the train/test generators.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -4,9 +4,14 @@ import os
 
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import VGG16
 
+from tqdm import tqdm
 
 DATA_DIR = os.path.join('../data')
+TRAIN_DIR = os.path.join(DATA_DIR, 'train/*')
+TRAIN_TARGET = os.path.join(DATA_DIR, 'train_labels.csv')
+TEST_DIR = os.path.join(DATA_DIR, 'test/*')
 
 
 class TrainDataGenerator(object):
@@ -98,3 +103,40 @@ class TestDataGenerator(object):
             _idx += self.batch_size
             batch_imgs = np.vstack(batch_imgs)
             yield preprocess_input(batch_imgs)
+
+
+def load_train_data():
+    targets = {}
+    with open(TRAIN_TARGET, 'r') as file_:
+        for line in file_:
+            _img, _prob = line.split(',')
+            targets[_img] = _prob
+
+    train_imgs = []
+    y_true = []
+
+    for train_img in tqdm(glob(TRAIN_DIR)):
+        img = image.load_img(train_img, target_size=(224, 224))
+        pixels = image.img_to_array(img=img)
+        train_imgs.append(np.expand_dims(pixels, axis=0))
+
+        img_name = train_img.split('/')[-1].split('.')[0]
+        y_true.append(targets[img_name])
+    return np.vstack(train_imgs), np.array(y_true)
+
+
+def load_test_data():
+    test_imgs = []
+
+    for test_img in tqdm(glob(TEST_DIR)):
+        img = image.load_img(test_img, target_size=(224, 224))
+        pixels = image.img_to_array(img=img)
+        test_imgs.append(np.expand_dims(pixels, axis=0))
+
+    return np.vstack(test_imgs)
+
+
+def bottleneck_features(tensor):
+    tensor = tensor.astype(float)/255
+    vgg16 = VGG16(include_top=False, weights='imagenet')
+    return vgg16.predict(preprocess_input(tensor))
